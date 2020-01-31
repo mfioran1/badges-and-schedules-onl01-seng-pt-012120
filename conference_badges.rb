5858e942c1ca3f42b7d9aafadbdf8e48fb276c4c
Maurice Fiorani <mauricef97@gmail.com>
2020-01-31 03:57:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,24 +1,4 @@
-def badge_maker(name)
-  "Hello, my name is #{name}."
-end
 
-def batch_badge_creator(attendees)
-  attendees.map do |attendees|
-    "Hello, my name is #{attendees}."
-  end
- 
- def assign_rooms
-  attendees.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
-  end
-  
-  def printer(attendees)
-    batch_badge_creator(attendees).each do |badge|
-      puts badge
-    end
-    assign_rooms(attendees).each do |person|
-      puts person
-    end
     
   
 end
