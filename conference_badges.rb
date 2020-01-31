4e74e8dadc8d0c8b117df9a1bcbc671f4609878c
Maurice Fiorani <mauricef97@gmail.com>
2020-01-31 04:24:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(attendees)
   attendees.map do |attendees, i|
-   "Hello, #{attendees}! You'll be assigned to room #{i}!"
+   "Hello, #{attendees}! You'll be assigned to room #{i+1}!"
  end
   
 end
