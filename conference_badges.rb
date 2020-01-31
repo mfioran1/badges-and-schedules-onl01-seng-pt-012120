422623482838611eb44e15984778f1338064a3cc
Maurice Fiorani <mauricef97@gmail.com>
2020-01-31 04:48:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,15 +16,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-batch_badge_creator(attendees).each do |badge|
-  puts badge
-end
-assign_rooms(attendees).each do |badge|
-  puts badge
+badges_and_room_assignments.each_line do |line|
+  puts line
 end
 
-  
-  
 end
 
   
